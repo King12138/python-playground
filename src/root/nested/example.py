2951255810b880dd9ce5840fadb9cccd1ec4f519
zhoujiagen <zhoujiagen@gmail.com>
2016-12-06 12:07:34 +0800

    init.

---
+++ src/root/nested/example.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2016-12-01 13:19:59
+
+@author: zhoujiagen
+'''
+
+import os
+
+if __name__ == '__main__':
+    import sys
+    print sys.version
+    env = os.environ
+    for key, value in env.items():
+        print key, "=", value
