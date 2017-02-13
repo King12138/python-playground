e488956c62ec6a8a7e6b42d727dad7a5f3b576ab
zhoujiagen <zhoujiagen@gmail.com>
2017-02-14 00:08:35 +0800

    fix extract Tweet data.

--- src/com/spike/text/nltk/twitter/twitter_entity.py
+++ src/com/spike/text/nltk/twitter/twitter_entity.py
@@ -31,25 +31,28 @@ def extract_tweet_info(tweet):
     result = {}
 
     user = tweet[u'user']
-    result['user'] = user[u'screen_name'].encode('utf8')
-    result['name'] = user[u'name'].encode('utf8')
-    result['location'] = user[u'location'].encode('utf8')
-    result['text'] = tweet[u'text'].encode('utf8')
+    result['user'] = _format(user[u'screen_name'].encode('utf8'))
+    result['name'] = _format(user[u'name'].encode('utf8'))
+    result['location'] = _format(user[u'location'].encode('utf8'))
+    result['text'] = _format(tweet[u'text'].encode('utf8'))
 
     entities = tweet[u'entities']
     hashtags = entities[u'hashtags']
     user_mentions = entities[u'user_mentions']
     urls = entities[u'urls']
 
-    result['hashtags'] = [hashtag[u'text'].encode('utf8') for hashtag in hashtags]
-    result['mentions'] = [um[u'screen_name'].encode('utf8') for um in user_mentions]
-    result['urls'] = [url[u'expanded_url'].encode('utf8') for url in urls]
+    result['hashtags'] = [_format(hashtag[u'text'].encode('utf8')) for hashtag in hashtags]
+    result['mentions'] = [_format(um[u'screen_name'].encode('utf8')) for um in user_mentions]
+    result['urls'] = [_format(url[u'expanded_url'].encode('utf8')) for url in urls]
 
     retweeted = tweet[u'retweeted']
     if retweeted:
         result['retweetuser'] = {'user': result['user'], 'name': result['name'], 'location': result['location']}
 
     return result
+
+def _format(string):
+    return string.replace('\'', '_').replace('"','')
 
 with open(input_file) as fp:
 #     json2csv(fp, 'extracted_tweets.txt',
